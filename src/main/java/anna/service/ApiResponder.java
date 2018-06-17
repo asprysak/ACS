9ef411760cf8fcbcbb97652f7aff6de95a03b8a6
Anna Sprysak <a.sprysak.pl@gmail.com>
2018-06-17 20:24:24 +0200

    extract the getResponse() method

--- src/main/java/anna/service/ApiResponder.java
+++ src/main/java/anna/service/ApiResponder.java
@@ -29,10 +29,7 @@ public class ApiResponder {
     public String[][] findAll() throws IOException {
 
         String url = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE=7&for=state:%20*&key=";
-        URL ad = new URL(url + key);
-        InputStreamReader reader = new InputStreamReader(ad.openStream());
-        String[][] response =  new Gson().fromJson(reader, String[][].class);
-        return response;
+        return getResponse(url + key);
     }
 
     public List<Integer> getHistData(String year, String state) throws IOException {
@@ -42,12 +39,10 @@ public class ApiResponder {
         for (int i = 3; i <= 25; i++ ) {
 
             String no = String.format("%02d", i);
-            String u = "https://api.census.gov/data/" + year + "/acs/acs1?get=NAME,B01001_0" + no + "E&for=state:"
+            String url = "https://api.census.gov/data/" + year + "/acs/acs1?get=NAME,B01001_0" + no + "E&for=state:"
                     + codes.get(state) + "&key=" + key;
 
-            URL ad = new URL(u);
-            InputStreamReader reader = new InputStreamReader(ad.openStream());
-            String[][] response = new Gson().fromJson(reader, String[][].class);
+            String[][] response = getResponse(url);
             population.add(Integer.parseInt(response[1][1]));
         }
         return population;
@@ -55,9 +50,15 @@ public class ApiResponder {
 
     public Map<String, String> cityCodes() throws IOException {
 
-        URL ad = new URL("https://api.census.gov/data/2016/cbp?get=GEO_TTL&for=state");
-        InputStreamReader reader = new InputStreamReader(ad.openStream());
-        String[][] codes =  new Gson().fromJson(reader, String[][].class);
+        String[][] codes = getResponse("https://api.census.gov/data/2016/cbp?get=GEO_TTL&for=state");
         return Arrays.stream(codes).skip(1).collect(Collectors.toMap(x -> x[0], x -> x[1]));
     }
+
+    private String[][] getResponse(String urlString) throws IOException {
+
+        URL url = new URL(urlString);
+        InputStreamReader reader = new InputStreamReader(url.openStream());
+        return new Gson().fromJson(reader, String[][].class);
+    }
+
 }
